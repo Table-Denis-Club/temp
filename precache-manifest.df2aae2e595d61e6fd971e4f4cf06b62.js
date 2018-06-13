7114edb0fda8ae5b4d2c510f3a380a5ea1570a85
Kang Cheng <kcheng@utilitywarehouse.co.uk>
2018-06-13 14:53:30 +0100

    init

---
+++ precache-manifest.df2aae2e595d61e6fd971e4f4cf06b62.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "ffc510c205d95c4c4a878ea9a7ac384d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1c50d94f2e9439d4b02",
+    "url": "/js/vendors~app.a1c50d94.js"
+  },
+  {
+    "revision": "74223cd4220130688caf",
+    "url": "/js/app.74223cd4.js"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "2e9a0313ed02aed1e206340feb36ec5c",
+    "url": "/fonts/Framework7Icons-Regular.2e9a0313.woff"
+  },
+  {
+    "revision": "ae8767ca8ecf4b090af58eff5dd6d3a7",
+    "url": "/fonts/Framework7Icons-Regular.ae8767ca.woff2"
+  },
+  {
+    "revision": "4348368ab857cb57b18e1a87ba8ff284",
+    "url": "/fonts/Framework7Icons-Regular.4348368a.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "0bd675f4127eb05561a4cdcf7bb578f6",
+    "url": "/img/ping-pong.0bd675f4.gif"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "c0087d4d5ddd32ec52f1859b42956db6",
+    "url": "/fonts/Framework7Icons-Regular.c0087d4d.eot"
+  },
+  {
+    "revision": "bf45922348a2d5f7efe62f64c249cd43",
+    "url": "/img/ionicons.bf459223.svg"
+  },
+  {
+    "revision": "5d92a3e14b32dea0b45c602270ed26db",
+    "url": "/fonts/ionicons.5d92a3e1.eot"
+  },
+  {
+    "revision": "854e2775c9a867fb0ad643dc6475642f",
+    "url": "/fonts/ionicons.854e2775.woff"
+  },
+  {
+    "revision": "a465c5c082eff8b410c67d5f72bc0878",
+    "url": "/fonts/ionicons.a465c5c0.woff2"
+  },
+  {
+    "revision": "95f6dc8d4e52a2a3d6a19a4b22157f36",
+    "url": "/fonts/ionicons.95f6dc8d.ttf"
+  },
+  {
+    "revision": "a1c50d94f2e9439d4b02",
+    "url": "/css/vendors~app.0.d727bf2c.css"
+  },
+  {
+    "revision": "74223cd4220130688caf",
+    "url": "/css/app.59d8eca0.css"
+  }
+];
